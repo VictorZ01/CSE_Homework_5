f6dab301286c17ae3fe0bc4f7ca3efa803260cc2
vzhen@machyna.com <victorzhen1vz@gmail.com>
2023-11-24 22:22:58 -0800

    replace

--- validation.js
+++ validation.js
@@ -2,9 +2,13 @@ const email = document.getElementById("email");
 
 email.addEventListener("input", (event) => {
   if (email.validity.typeMismatch) {
-    email.setCustomValidity("I am expecting an email address!");
+    email.setCustomValidity("Please Enter a valid email address");
   } else {
     email.setCustomValidity("");
   }
 });
 
+document.getElementById('comments').addEventListener("input",function(){
+    const reg=/[^a-zA-Z0-9.,?! ]/g;
+    this.value=this.value.replace(reg,'')
+});
