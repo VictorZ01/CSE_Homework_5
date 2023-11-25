63ee177b50319d9be0eb1a7f2c2d0e68c69c3d85
vzhen@machyna.com <victorzhen1vz@gmail.com>
2023-11-24 23:28:25 -0800

    flashing

--- validation.js
+++ validation.js
@@ -1,4 +1,5 @@
 const email = document.getElementById("email");
+var flash = document.querySelector('.flash');
 
 email.addEventListener("input", (event) => {
   if (email.validity.typeMismatch) {
@@ -8,10 +9,14 @@ email.addEventListener("input", (event) => {
   }
 });
 let regex_name= /[A-Za-z]/;
+
+let regex_email= /[A-Za-z@._]/;
+
 const filter=(event,regex_name)=>{
     let symbol = String.fromCharCode(event.keyCode);
   if (!regex_name.test(symbol)) {
     event.preventDefault();
+    flash.classList.add('flash-is-showing');
   }
 };
 
