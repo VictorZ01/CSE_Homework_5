80bdc08e7f1ee786c3debd332c5bd02579d153d2
vzhen@machyna.com <victorzhen1vz@gmail.com>
2023-11-25 16:29:14 -0800

    theme

--- validation.js
+++ validation.js
@@ -56,32 +56,25 @@ const names = document.getElementById("name");
 
 names.addEventListener('keypress',inputEvent);
 
-const theme = document.getElementById('theme');
-const changeTheme = document.getElementById('mode');
-changeTheme.onchange = (e) => {
-  if (changeTheme.checked === true) {
-    console.log("Checked")
-    theme.innerHTML="Light Theme"
-    document.documentElement.classList.remove("dark")
-    document.documentElement.classList.add("light")
-    window.localStorage.setItem('mode', 'light');
-  } else {
-    console.log("Not Checked")
-    theme.innerHTML="Dark Theme"
-    document.documentElement.classList.remove("light")
-    document.documentElement.classList.add("dark")
-    window.localStorage.setItem('mode', 'dark');
-  }
-}
-const mode = window.localStorage.getItem('mode');
-if (mode == 'dark') {
-  changeTheme.checked = true;
-  document.documentElement.classList.remove("light")
-  document.documentElement.classList.add("dark")
-}
+function setTheme(theme) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+};
 
-if (mode == 'light') {
-  changeTheme.checked = false;
-  document.documentElement.classList.remove("dark")
-  document.documentElement.classList.add("light")
-}
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+};
+
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+        document.getElementById('slider').checked = false;
+    } else {
+        setTheme('theme-light');
+      document.getElementById('slider').checked = true;
+    }
+})();
