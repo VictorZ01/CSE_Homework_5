1a469a14e140cabdc1cc40bc4ee8e01b03721a21
vzhen@machyna.com <victorzhen1vz@gmail.com>
2023-11-25 16:16:41 -0800

    checking

--- validation.js
+++ validation.js
@@ -56,15 +56,32 @@ const names = document.getElementById("name");
 
 names.addEventListener('keypress',inputEvent);
 
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme) {
-  document.body.classList.add(currentTheme);
+const theme = document.getElementById('theme');
+const changeTheme = document.getElementById('mode');
+changeTheme.onchange = (e) => {
+  if (changeTheme.checked === true) {
+    console.log("Checked")
+    theme.innerHTML="Light Theme"
+    document.documentElement.classList.remove("dark")
+    document.documentElement.classList.add("light")
+    window.localStorage.setItem('mode', 'light');
+  } else {
+    console.log("Not Checked")
+    theme.innerHTML="Dark Theme"
+    document.documentElement.classList.remove("light")
+    document.documentElement.classList.add("dark")
+    window.localStorage.setItem('mode', 'dark');
+  }
+}
+const mode = window.localStorage.getItem('mode');
+if (mode == 'dark') {
+  changeTheme.checked = true;
+  document.documentElement.classList.remove("light")
+  document.documentElement.classList.add("dark")
 }
 
-
-function toggleTheme() {
-  const body = document.body;
-  body.classList.toggle('dark-mode');
-  const theme = body.classList.contains('dark-mode') ? 'dark-mode' : '';
-  localStorage.setItem('theme', theme);
+if (mode == 'light') {
+  changeTheme.checked = false;
+  document.documentElement.classList.remove("dark")
+  document.documentElement.classList.add("light")
 }
